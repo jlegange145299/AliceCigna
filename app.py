f36da41e5800ebee9f320af39112293054b1fb5a
Jacques Le Grange <jacqueslegrange@gmail.com>
2024-04-01 16:16:05 +0400

    Update app.py
    #out immediate audio

--- app.py
+++ app.py
@@ -182,9 +182,9 @@ def main():
                 audio_file.write(response.content)
 
 # Play the most recent audio file
-            #st.audio(speech_file_path.read_bytes())
-            audio_file_path = "speech.mp3"
-            st.markdown(f'<audio autoplay controls><source src="{audio_file_path}" type="audio/mp3"></audio>',unsafe_allow_html=True)
+            st.audio(speech_file_path.read_bytes())
+            #audio_file_path = "speech.mp3"
+            #st.markdown(f'<audio autoplay controls><source src="{audio_file_path}" type="audio/mp3"></audio>',unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
